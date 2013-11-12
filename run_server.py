6171937dab8852376641ab640fc91a943b180cbe
John Schimmel <john.schimmel@dowjones.com>
2013-11-12 13:56:33 -0500

    app port

--- run_server.py
+++ run_server.py
@@ -1,3 +1,4 @@
+import os
 from app import app
 from notes import notes_app
 from auth import auth_flask_login
@@ -6,4 +7,5 @@ app.register_blueprint(notes_app)
 app.register_blueprint(auth_flask_login)
 
 if __name__ == "__main__":
-	app.run()
+	port = int(os.environ.get('PORT', 5000)) # locally PORT 5000, Heroku will assign its own port
+	app.run(host='0.0.0.0', port=port)
